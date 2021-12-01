643514c5f975e7b4a5da3c39438412e023ae0772
doyon <doyon3x3@gmail.com>
2021-12-02 03:03:12 +0900

    show the game score

---
+++ Score.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static int score = 0;
+
+    // Update is called once per frame
+    void Update() 
+    {
+        GetComponent<Text>().text = score.ToString();
+    }
+}
