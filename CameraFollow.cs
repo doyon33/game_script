759fc7d732e7be34e7f38fcc325b274bcfd4957b
doyon <doyon3x3@gmail.com>
2021-12-06 22:51:29 +0900

    more smoother

--- CameraFollow.cs
+++ CameraFollow.cs
@@ -1,22 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
+// using System.Collections;
+// using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    GameObject player;
+    // GameObject player;
+    // public Transform target;
+    // public float Xmin;
+    // public float Xmax;
+
+    // void Start()
+    // {
+    //     player = GameObject.Find("Player");
+    // }
+
+    // void Update()
+    // {
+    //     Vector3 PlayerPos = player.transform.position;
+    //     transform.position = new Vector3(PlayerPos.x, transform.position.y, transform.position.z);
+    // }
     public Transform target;
-    public float Xmin;
-    public float Xmax;
+    public Vector3 offset;
+    [Range(1, 10)]
+    public float smoothFactor;
 
-    void Start()
-    {
-        player = GameObject.Find("Player");
+    private void FixedUpdate() {
+        Follow();
     }
-
-    void Update()
+    void Follow()
     {
-        Vector3 PlayerPos = player.transform.position;
-        transform.position = new Vector3(PlayerPos.x, transform.position.y, transform.position.z);
+        Vector3 targetPosition = target.position + offset;
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, targetPosition, smoothFactor*Time.fixedDeltaTime);
+        transform.position = smoothPosition;
     }
 }
